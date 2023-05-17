6b363e1949f78cc397248d586e91142dc3997f63
lokesh62 <lokeshpentyala1999@gmail.com>
2023-05-17 10:50:07 -0500

    commiting python patterns

---
+++ patterns.py
@@ -0,0 +1,76 @@
+# for i in range(4):
+#     for j in range(i + 1):
+#         print("#",end=" ")
+#     print()
+
+    #
+    # #
+    # # #
+    # # # #
+
+
+# for i in range(4):
+#     for j in range(4-i):
+#         print("#", end=" ")
+#     print()
+
+    # # # #
+    # # #
+    # #
+    #
+
+# for i in range(5):
+#     for j in range(6-i):
+#         print(j, end=" ")
+#     print()
+
+# 0 1 2 3 4 5
+# 0 1 2 3 4
+# 0 1 2 3
+# 0 1 2
+# 0 1
+
+
+# name = "python"
+# for i in range(1):
+#     for j in range(1):
+#         print(name[0])
+#     for j in range(1):
+#         print(name[0],name[1])
+#     for k in range(1):
+#         print(name[0],name[1],name[2])
+#     for l in range(1):
+#         print(name[0],name[1],name[2],name[3])
+#     for a in range(1):
+#         print(name[0],name[1],name[2],name[3],name[4])
+#     for b in range(1):
+#         print(name[0],name[1],name[2],name[3],name[4],name[5])
+
+# p
+# p y
+# p y t
+# p y t h
+# p y t h o
+# p y t h o n
+
+
+# name = "python"
+# for i in range(1, len(name)+1):
+#     print(' '.join(name[:i]))
+
+# p
+# p y
+# p y t
+# p y t h
+# p y t h o
+# p y t h o n
+
+
+
+
+
+
+
+
+
+
